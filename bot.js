7bc141cbd49f24ceef7a7d535723c707aca36bd9
hshshsasad <61587448+hshshsasad@users.noreply.github.com>
2020-02-28 20:21:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
-const myid = [''];
+const myid = ['681120889135562758'];
 const prefix = ['-'];
 
 client.on('ready', () => {
